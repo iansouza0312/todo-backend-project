09bfb142d9bed70f8006730ef703043c97b4c97e
Ian Souza <ianfmsouza@gmail.com>
2023-08-03 17:15:07 -0300

    generate the project

---
+++ src/test/java/br/iafesou/todolistproject/TodoListProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.iafesou.todolistproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
