4671f1c975477447637d313fcf3eaae44c3b5f8f
Ian Souza <ianfmsouza@gmail.com>
2023-08-06 19:02:47 -0300

    create the first automated test of POST method.

--- src/test/java/br/iafesou/todolistproject/TodoListProjectApplicationTests.java
+++ src/test/java/br/iafesou/todolistproject/TodoListProjectApplicationTests.java
@@ -1,13 +1,40 @@
 package br.iafesou.todolistproject;
 
+import br.iafesou.todolistproject.model.Task;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TodoListProjectApplicationTests {
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
-	void contextLoads() {
+	void testCreateTaskSuccess() {
+		var taskBody = new Task("first task", "some description", false, 2);
+
+		webTestClient.post().uri("/api/tasks").bodyValue(taskBody).exchange().expectStatus().isOk().expectBody()
+				.jsonPath("$").isArray()
+				.jsonPath("$.length()").isEqualTo(1)
+				.jsonPath("[0].name").isEqualTo(taskBody.getName())
+				.jsonPath("[0].description").isEqualTo(taskBody.getDescription())
+				.jsonPath("[0].done").isEqualTo(taskBody.isDone())
+				.jsonPath("[0].priority").isEqualTo(taskBody.getPriority());
+	}
+	@Test
+	void testCreateTaskError() {
+
 	}
 
+
+	@Test
+	void testDeleteTaskSuccess() {
+
+	}
+	@Test
+	void testDeleteTaskError() {
+
+	}
 }
