027af02be0dfdb7ce238f02b0e2b2acac9105c74
Ian Souza <ianfmsouza@gmail.com>
2023-08-06 19:18:10 -0300

    setting validation methods for the fields of the entity.

--- src/main/java/br/iafesou/todolistproject/model/Task.java
+++ src/main/java/br/iafesou/todolistproject/model/Task.java
@@ -1,6 +1,7 @@
 package br.iafesou.todolistproject.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "tasks")
@@ -8,7 +9,9 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String name;
+    @NotBlank
     private String description;
     private boolean isDone;
     private int priority;
